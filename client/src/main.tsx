c8d64fc4de0fc89199b53f2087db204e2e9f8a14
samanyuahuja <samanyuahuja@gmail.com>
2026-08-11 06:49:38 +0530

    Restore original NephroCare typography

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "@fontsource-variable/manrope";
-import "@fontsource-variable/newsreader";
 import "lenis/dist/lenis.css";
 import "./index.css";
 
